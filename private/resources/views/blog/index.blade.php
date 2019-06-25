f27f471945ca5f7dfe28aada293b8ab67c4d62d6
royhan31 <izzapark34@gmail.com>
2019-06-25 13:19:30 +0700

    first commit

---
+++ private/resources/views/blog/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="text-right mb-4">
+    <button class="btn btn-primary" type="button" onclick="window.location=''" name="button">Tambah</button>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <div class="card">
+        <div class="card-title">
+
+        </div>
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
